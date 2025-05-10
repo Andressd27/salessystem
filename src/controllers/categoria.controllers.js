fc9d5ad60aa9e6420283d7b4b89961475c56b3ec
Andres Duran <aduran@tiresia.co>
2025-05-09 19:29:43 -0500

    destructuro el json con rows

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -3,11 +3,9 @@ import getConnection from "./../db/database.js"
 const getCategorias = async (req, res) => {
     try {
         const connection = await getConnection();
-        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
-        res.json(result);
+        const [rows] = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
+        res.json(rows);
     } catch (error) {
-        console.log("error",error);
-        
         console.error("Not possible connection database");
     }
 }
